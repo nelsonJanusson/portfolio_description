0ec2b3d1022d04055c80a5401cb0d493db014c58
nelson <hejnelson@gmail.com>
2025-03-24 09:50:40 +0100

    add script

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,27 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
 
+# Install cilium
+helm install cilium cilium/cilium \
+  --version 1.17.2 \
+  --namespace kube-system \
+  --set operator.replicas=1 \
+  --set kubeProxyReplacement=true \
+  --set serviceMesh.enabled=true \
+  --set hubble.enabled=true \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true \
+  --set gatewayAPI.enabled=true
+
+# Install cilium CLI
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
 # Install CloudNativePG operator
 helm install cnpg cnpg/cloudnative-pg \
   --namespace cnpg-system \
@@ -41,26 +62,6 @@ until [[ $(kubectl get endpoints cnpg-webhook-service -n cnpg-system -o jsonpath
   sleep 5
 done
 
-# Install cilium
-helm install cilium cilium/cilium \
-  --version 1.17.2 \
-  --namespace kube-system \
-  --set operator.replicas=1 \
-  --set kubeProxyReplacement=true \
-  --set serviceMesh.enabled=true \
-  --set hubble.enabled=true \
-  --set hubble.relay.enabled=true \
-  --set hubble.ui.enabled=true \
-  --set gatewayAPI.enabled=true
-
 # Install custom helm charts  
 helm install application-deployment custom-chart-repo/application-deployment
 
-# Install cilium CLI
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
